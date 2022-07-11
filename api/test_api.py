cef6aca9aa27ced76f0e64abdd2ebb0df007f1ce
Aravind E B <96783291+aravind-etagi@users.noreply.github.com>
2022-07-11 17:14:16 +0530

    Updating file_path error

--- api/test_api.py
+++ api/test_api.py
@@ -2,7 +2,7 @@ import requests
 
 #Define API and Input
 url = 'http://127.0.0.1:8000/predict/image'
-file_path = './images/MINI Cooper Roadster Convertible 2012.jpg'
+file_path = '../images/MINI Cooper Roadster Convertible 2012.jpg'
 
 #Prepare Input
 files = {'file': (file_path, open(file_path, 'rb'), "image/jpeg")}
