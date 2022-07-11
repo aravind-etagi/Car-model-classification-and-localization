9d634d5490ee8397dcdce0779fa73efa87ee50fd
Aravind E B <96783291+aravind-etagi@users.noreply.github.com>
2022-07-11 16:49:05 +0530

    Add files via upload

---
+++ api/test_api.py
@@ -0,0 +1,12 @@
+import requests
+
+#Define API and Input
+url = 'http://127.0.0.1:8000/predict/image'
+file_path = './images/MINI Cooper Roadster Convertible 2012.jpg'
+
+#Prepare Input
+files = {'file': (file_path, open(file_path, 'rb'), "image/jpeg")}
+response = requests.post(url, files=files)
+
+print('status code : ',response.status_code)
+print(response.json())
